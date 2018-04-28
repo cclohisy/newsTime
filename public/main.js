ccbc949db79305d4dce9287b8ab1c009a3613d64
Cecilia Clohisy <cclohisy@gmail.com>
2018-04-28 17:12:09 -0500

    delete button working

--- public/main.js
+++ public/main.js
@@ -8,13 +8,36 @@ $(function () {
         //grab id from button
         var articleId = $(this).attr("article-id")
         console.log("article id: " + articleId)
-//run put ajax to update save value to true
+        //run put ajax to update save value to true
         $.ajax("/article/save/" + articleId,
             {
                 type: "PUT",
             }
         ).then(function () {
             location.reload()
-        })
+        }).catch(function (err) { console.log(err) })
+    })
+
+    // on click of delete button... 
+    $(".deleteArticle").on("click", function (event) {
+        event.preventDefault()//? still need to be doing this?
+        console.log("delet4e click workin")
+        //grab id of article to delete
+        var articleId = $(this).attr("article-id")
+        console.log("id to delete: " + articleId)
+        //run ajax call to delete route
+        $.ajax("/article/delete/" + articleId,
+            {
+                type: "PUT"
+            }).then(function () {
+                location.reload()
+            }).catch(function (err) { console.log(err) })
+    })
+
+
+    // on click of comment button... 
+    $(".commentsBtn").on("click", function (event) {
+        event.preventDefault()//? still need to be doing this?
+        console.log("comment click workin")
     })
 })
