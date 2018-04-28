422a4d578751508bf59cc017746e1333d2a84e66
Cecilia Clohisy <cclohisy@gmail.com>
2018-04-28 12:58:44 -0500

    index hbs synatax working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,30 @@
+var db = require("../models");
+
+
 module.exports = function (app) {
     //route to load index... get articles from DB
     // display data using handlebars
-    app.get("/", function(req,res){
-        // db.Article.find({}).then(function)
-        res.render("index")
+    app.get("/", function (req, res) {
+        db.Article.find({
+        }).then(
+            function (articleData) {
+                // console.log("article object____________________",articleData)
+                var articleObj = {
+                    article: articleData
+                }
+                console.log("articleObj", articleObj.article)
+                for (var i = 0; i < articleObj.article.length; i++) {
+                    console.log(i + " article info________________________")
+                    console.log("headline: " + articleObj.article[i].headline)
+                    console.log("summary: " + articleObj.article[i].summary)
+                    console.log("link: " + articleObj.article[i].link)
+                    console.log("headline: " + articleObj.article[i].comments)
+                }
+                res.render("index", articleObj)
+
+                // res.json(dbArticle)
+            }).catch(function (err) {
+                res.json(err)
+            })
     })
 }
