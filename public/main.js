6939294f08e6b717c2fda3add5bc4d0116fd55d8
Cecilia Clohisy <cclohisy@gmail.com>
2018-04-29 15:55:33 -0500

    jquery display comments

--- public/main.js
+++ public/main.js
@@ -40,10 +40,32 @@ $(function () {
         event.preventDefault()//? still need to be doing this?
         console.log("comment click workin")
         //get and store id of article var
-        
+        var articleId = $(this).attr("article-id")
         //ajax get exsisting comments... 
+        $.ajax("/article/comments/" + articleId,
+            {
+                type: "GET"
+            }).then(function (data) {
+                console.log(data)
+                 //display results in commentModal
+                //append headline
+                $("#articleHeadline").append(data[0].headline)
+                // for loop to display exsisting comments
+                var commArray = data[0].comments
+                for(var i = 0; i< commArray.length; i++){
+                    var comment = commArray[i].content
+                    var listItem = $("<li>")
+                    listItem.addClass("list-group-item")
+                    var deleteBtn = $("<button>")
+                    deleteBtn.addClass("btn float-right deleteCommBtn")
+                    deleteBtn.text("X")
+                    listItem.append(deleteBtn)
+                    listItem.append(comment)
+                    $("#commentList").append(listItem)
+                    
+                }
 
-        //display results in commentModal
+            })
 
         //toggle modal 
         $("#commentModal").modal("toggle")
