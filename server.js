21e3c6f86e1a9415e55fd060f388a293328b795c
Cecilia Clohisy <cclohisy@gmail.com>
2018-04-30 18:04:39 -0500

    added monogoLab

--- server.js
+++ server.js
@@ -45,7 +45,17 @@ require("./routes/api-routes.js")(app)
 require("./routes/html-routes.js")(app)
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines");
+//mongoose.connect("mongodb://localhost/mongoHeadlines");
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 
 // Start the server
