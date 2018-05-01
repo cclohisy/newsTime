84aad28c85d16fc1272df8414853baf9046c2c72
Cecilia Clohisy <cclohisy@gmail.com>
2018-05-01 14:52:01 -0500

    file change

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 //export all the models
 
 module.exports = {
-    Article :   require("./Article"),
-    Comment:       require("./Comms")
+    Article :   require("./article"),
+    Comment:       require("./comms")
 }
